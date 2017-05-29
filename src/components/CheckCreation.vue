2c65d8f90bc83b4cebd03adba54aca9f24e7e16c
Roberto Vargas C <rvargas@gpshiring.com>
2017-05-29 15:18:46 -0600

    merge

--- src/components/CheckCreation.vue
+++ src/components/CheckCreation.vue
@@ -420,15 +420,16 @@ export default {
       this.addressLine2View = !this.addressLine2View;
     },
     postToChekApi() {
-      this.$http.post('localhost:3000/checks', this.request).then((response) => {
+      this.$http.post('/checks', this.request).then((response) => {
         response.headers.get('Expires');
         this.someData = response.body;
       }, (response) => {
         this.someData = response.body;
       });
-  },
-  createEaselComponent() {
+    },
+    createEaselComponent() {
 
+    },
   },
 };
 </script>
